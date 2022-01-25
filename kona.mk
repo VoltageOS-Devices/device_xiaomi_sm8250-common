445475bbe34474aeb0cb2423db0a1169b77d8b50
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    sm8250-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- kona.mk
+++ kona.mk
@@ -103,7 +103,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.3-impl
 
@@ -149,6 +148,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
