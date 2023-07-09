873dadc20b5580dce22295738892428ba8a4f6ce
AnierinB <anierin@evolution-x.org>
2023-07-09 19:22:54 +0530

    sm8250-common: parts: Add KCAL display Calibration
    Pranav-Talmale: Adapt for XiaomiParts
    
    Change-Id: Idf329a178f931ea2e834a57c5c87b931888256b0
    Co-authored-by: Pranav-Talmale <pranavtalmale@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>
    Signed-off-by: Pranav-Talmale <pranavtalmale@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,6 +32,7 @@ import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.preferences.*;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -58,6 +59,13 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
         FileUtils.enableService(context);
+        BluePreference.restore(context);
+        ContrastPreference.restore(context);
+        GreenPreference.restore(context);
+        HuePreference.restore(context);
+        RedPreference.restore(context);
+        SaturationPreference.restore(context);
+        ValuePreference.restore(context);
 
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
